8b7c64c6620c7cb788ab1aabc6a8f09cc7936f42
Abbey Sparrow <abbey@khrome.net>
2020-09-29 08:03:23 -0600

    layers

--- engines/mapbox.js
+++ engines/mapbox.js
@@ -1,6 +1,34 @@
+var polyMask = function(features, turf){
+    var featureCollection = features.reduce(function(featureCollection, feature){
+        var geojsonType = turf.getType(feature);
+        switch(geojsonType){
+            case 'Polygon':
+                featureCollection.push(turf.polygon(feature.geometry.coordinates));
+                break;
+            case 'MultiPolygon':
+                featureCollection.push(turf.multiPolygon(feature.geometry.coordinates));
+                break;
+            default: break;
+        }
+        return featureCollection;
+    }, []);
+    var mask = turf.combine(turf.featureCollection(featureCollection)).features[0];
+    var bboxPoly = turf.bboxPolygon([-180, -90, 180, 90]);
+    var res;
+    if(Array.isArray(mask)){
+        mask.forEach(function(feature){
+            res = turf.difference(res || bboxPoly, feature);
+        });
+    }else{
+        res = turf.difference(bboxPoly, mask);
+    }
+    return res;
+};
+
 var mb = {
-    requireDependencies : function(){
-        if(!window.mapboxgl) throw new Error('mapbox required')
+    requireDependencies : function(auth){
+        if(!window.mapboxgl) throw new Error('mapbox required');
+        window.mapboxgl.accessToken = auth.token;
     },
     createMap : function(options){
         var map = new mapboxgl.Map({
@@ -9,15 +37,154 @@ var mb = {
             center: options.center || [-74.5, 40], // starting position [lng, lat]
             zoom: options.zoom || 9 // starting zoom
         });
+        map.waitingShapeLayers = {};
 
         return map;
     },
-    createLayer : function(){
+    createLayer : function(map, options){
+        if(options.tiles){
+            return {
+                "id": 'tiles-layer',
+                "type": "raster",
+                "source": {
+                    type : "raster",
+                    tileSize : (options.size || 256),
+                    "raster-fade-duration" : (options.transitionDuration || 500),
+                    tiles : [
+                    	options.tiles
+                    ]
+                }
+            }
+        }
+        if(options.data){ //we're displaying shapes
+            var map = {
+                '$eq' : '=='
+            }
+            var filter;
+            if(options.filter && options.filter.properties){
+                filter = ['all'];
+                Object.keys(options.filter.properties).forEach(function(propertyName){
+                    var operation = Object.keys(
+                        options.filter.properties[propertyName]
+                    )[0];
+                    if(!map[operation]) throw new Error('Unknown Operation: '+operation);
+                    filter.push([
+                        map[operation],
+                        propertyName,
+                        options.filter.properties[propertyName][operation]
+                    ]);
+                });
+                delete options.filter.properties;
+            }
+            var layers = [];
+            if(options['fill-color']){
+                layers.push({
+                    'id': options.name+'-fill',
+                    'type': 'fill',
+                    'source': options.data,
+                    'layout': {},
+                    'filter': filter,
+                    'paint': {
+                        'fill-color': (options['fill-color'] || '#333333'),
+                        'fill-opacity': (options['fill-opacity'] || .8)
+                    }
+                });
+            }
+            if(options['stroke-color']){
+                layers.push({
+                    'id': options.name+'-line',
+                    'type': 'line',
+                    'source': options.data,
+                    'layout': {},
+                    'filter': filter,
+                    'paint': {
+                        'line-color': (options['stroke-color'] || '#000000'),
+                        'line-opacity': (options['stroke-opacity'] || 1),
+                        'line-width': (options['stroke-width'] || 4)
+                    }
+                });
+            }
+            if(options['text'] || options['property']){
+                layers.push({
+                    'id': options.name+'-text',
+                    'type': 'symbol',
+                    'source': options.data,
+                    'layout': {
+                        'text-field': (
+                            options['property'] && options['property']+' : {'+options['property']+'}'
+                        ) || options['text'],
+                        //'text-size': (options['text-size'] || 1),
+                        "text-font": [(options['text-font'] || 'Arial Unicode MS Regular')],
+                    },
+                    'filter': filter,
+                    minzoom:14,
+                    'paint': {
+                        'text-color': (options['text-color'] || 'rgb(0,0,0)'),
+                    }
+                });
+            }
+            console.log(layers)
+            return layers;
+        }
+    },
+    addLayer : function(map, layer, options){
+        var ob = this;
+        if(Array.isArray(layer)){
+            layer.forEach(function(sublayer){
+                ob.addLayer(map, sublayer, options);
+            })
+        }else{
+            if(layer.type === 'fill'){
+                if(options.callback){
+                    map.on('click', layer.id, function(e){
+                        if(window[options.callback]){
+                            window[options.callback](
+                                e.features[0],
+                                function(coordinates, description){
+                                    new mapboxgl.Popup()
+                                    .setLngLat(coordinates)
+                                    .setHTML(description)
+                                    .addTo(map);
+                                }
+                            )
+                        }
+                    });
+
+                    // Change the cursor to a pointer when the mouse is over the places layer.
+                    map.on('mouseenter', layer.id, function () {
+                        map.getCanvas().style.cursor = 'pointer';
+                    });
+
+                    // Change it back to a pointer when it leaves.
+                    map.on('mouseleave', layer.id, function () {
+                        map.getCanvas().style.cursor = '';
+                    });
+                }
+            }
+            if(layer){
+                map.addLayer(layer);
+            }else{
+                console.log('layer not added');
+            }
+        }
+    },
+    removeLayer : function(map, layer){
 
     },
-    createData : function(){
-
+    createData : function(name, dt, options){
+        var data = (typeof dt === 'string')?JSON.parse(dt):dt;
+        // data is either an array of features or a FeatureCollection
+        // return is always a FeatureCollection
+        //if(options.mask)
+        var collection = Array.isArray(data)?{
+            "type": "FeatureCollection",
+            "features": (options.mask && options.turf)?polyMask(data, options.turf):data
+        }:data;
+        return {type:'geojson', data:collection};
     },
+    addData : function(map, name, data){
+        map.addSource(name, data);
+    }
 }
 try{
     module.exports = mb;
--- keyhole.js
+++ keyhole.js
@@ -12,10 +12,23 @@ var setOptIfAttr = function(name, options, el){
 class SkeletonKeyhole extends HTMLElement{
     constructor(){
       super();
+      var jobs = [];
+      this.ready = function(handler){
+          if(!jobs) return handler();
+          if(typeof handler === 'boolean'){
+              jobs.forEach(function(job){
+                  job();
+              });
+              jobs = false;
+          }else{
+              jobs.push(handler)
+          }
+      }
     }
     connectedCallback(){
         var el = document.createElement('div');
-        el.setAttribute('id', 'dkjhdfskjfh')
+        var id = 'dkjhdfskjfh'
+        el.setAttribute('id', id);
         this.appendChild(el);
 
         var engineName = this.getAttribute('engine');
@@ -26,11 +39,36 @@ class SkeletonKeyhole extends HTMLElement{
         }catch(ex){
             throw new Error('engine not found: '+engineName);
         }
-        engine.requireDependencies();
+        var auth = {};
+        var authToken = this.getAttribute('token');
+        if(authToken) auth.token = authToken;
+        engine.requireDependencies(auth);
         var options = {};
         setOptIfAttr('zoom', options, this);
         setOptIfAttr('center', options, this);
-
+        options.id = id;
+        this.engineInstance = engine;
+        var map = engine.createMap(options);
+        this.mapInstance = map;
+        var ob = this;
+        map.on('load', function(){
+            ob.ready(true);
+        });
+        this.addEventListener('keyhole-layer-add', function(e){
+            ob.ready(function(){
+                e.el.connectToMap(map, engine);
+            });
+        });
+        this.addEventListener('keyhole-data-add', function(e){
+            ob.ready(function(){
+                e.el.connectToMap(map, engine);
+            });
+        });
+        this.addEventListener('keyhole-shapes-add', function(e){
+            ob.ready(function(){
+                e.el.connectToMap(map, engine);
+            });
+        });
     }
     disconnectedCallback(){
 
@@ -42,8 +80,26 @@ class KeyholeTileLayer extends HTMLElement{
     constructor(){
       super();
     }
+    getKeyhole(){
+        var node = this;
+        while(!node instanceof SkeletonKeyhole){
+            node = node.parentNode;
+        }
+        return node;
+    }
     connectedCallback(){
-        this.getAttribute('');
+        var event = new Event('keyhole-layer-add', {bubbles: true});
+        event.el = this;
+        this.dispatchEvent(event);
+    }
+    connectToMap(map, engine){
+        if(!this.instance){
+            var url = this.getAttribute('url')
+            this.instance = engine.createLayer(map, {
+                tiles : url
+            }, {});
+        }
+        engine.addLayer(map, this.instance);
     }
     disconnectedCallback(){
 
@@ -56,10 +112,83 @@ class KeyholeDataLayer extends HTMLElement{
       super();
     }
     connectedCallback(){
+        var event = new Event('keyhole-data-add', {bubbles: true});
+        event.el = this;
+        this.dispatchEvent(event);
         this.getAttribute('');
+    }
+    connectToMap(map, engine){
+        var name = this.getAttribute('name');
+        if(!this.data){
+            var data = this.getAttribute('data');
+            if(data){
+                try{
+                    data = JSON.parse(data);
+                }catch(ex){} //must be a string?
+            }
+            var mask;
+            try{ mask = JSON.parse(this.getAttribute('mask')) }catch(ex){}
+            this.data = engine.createData(name, data, {
+                turf: this.turf,
+                mask: mask
+            });
+        }
+        //todo: support data fetch
+        engine.addData(map, name, this.data);
     }
     disconnectedCallback(){
 
     }
 }
 customElements.define('keyhole-data', KeyholeDataLayer);
+
+var getValue = function(el, name){
+    try{ return JSON.parse(el.getAttribute(name)) }catch(ex){}
+}
+
+class KeyholeShapesLayer extends HTMLElement{
+    constructor(){
+      super();
+    }
+    connectedCallback(){
+        var event = new Event('keyhole-shapes-add', {bubbles: true});
+        event.el = this;
+        this.dispatchEvent(event);
+    }
+    connectToMap(map, engine){
+        var name = this.getAttribute('name');
+        var filter = {};
+        if(name){
+            try{
+                filter = JSON.parse(this.getAttribute('filter'));
+            }catch(ex){}
+            var data = this.getAttribute('data');
+            var options = {
+                data,
+                name,
+                filter,
+                'fill-color': this.getAttribute('fill-color'),
+                'fill-opacity': getValue(this, 'fill-opacity'),
+                'text': this.getAttribute('text'),
+                'property': this.getAttribute('property'),
+                'callback': this.getAttribute('callback'),
+                'hover': this.getAttribute('hover'),
+                'text-color': this.getAttribute('text-color'),
+                'text-font': this.getAttribute('text-font'),
+                'stroke-color': this.getAttribute('stroke-color'),
+                'stroke-width': getValue(this, 'stroke-width'),
+                'stroke-opacity': getValue(this, 'stroke-opacity')
+            };
+            if(data){
+                this.layer = engine.createLayer(map, options);
+            }
+        }
+        if(!this.layer) throw new Error('No Layer!!');
+        //todo: support data fetch
+        engine.addLayer(map, this.layer, options);
+    }
+    disconnectedCallback(){
+
+    }
+}
+customElements.define('keyhole-shapes', KeyholeShapesLayer);
